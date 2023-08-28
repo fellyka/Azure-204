66bfd646ec4f6bc43489a2d819d653e1a7465177
fellly <fellyka@gmail.com>
2023-08-28 14:36:08 +0200

    Add project files.

---
+++ AzureBlobStorage/AzureBlobStorage/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Azure.Storage.Blobs;
+
+namespace AzureBlobStorage
+{
+    public class Program
+    {
+        
+        private static string _connectionString = "DefaultEndpointsProtocol=https;" +
+            "AccountName=stsollerspowershell;" +
+            "AccountKey=bIddy5e7J0keJrTcGkCiHYKfokkxz7RaelBhFiaLxUQ5R7r7LQnZ3z4/Qh67mPICL8bTeK3S2xnh+AStNdU/QQ==;" +
+            "EndpointSuffix=core.windows.net";
+        private static string _containerName = "fellyka";
+        static void Main(string[] args)
+        {
+            //Create a containner
+            BlobServiceClient blobServiceClient = new BlobServiceClient(_connectionString);
+            blobServiceClient.CreateBlobContainer(_containerName);
+            Console.WriteLine("Container succesfully created!");
+        }
+    }
+    
+}
